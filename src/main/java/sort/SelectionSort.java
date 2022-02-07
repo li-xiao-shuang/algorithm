5bfe96886151634bd7b7fd5d31b8c250f5ac52d4
lixiaoshuang <644968328@qq.com>
2022-02-07 15:57:08 +0800

    selection sort

---
+++ src/main/java/sort/SelectionSort.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2021 Gypsophila open source organization.
+ *
+ * Licensed under the Apache License,Version2.0(the"License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sort;
+
+/**
+ * 选择排序
+ *
+ * @author lixiaoshuang
+ */
+public class SelectionSort {
+    
+    public static void main(String[] args) {
+        int[] data = {20, 40, 30, 10, 60, 50};
+        System.out.println("排序前：");
+        for (int datum : data) {
+            System.out.println(datum);
+        }
+        
+        int[] sort = sort(data);
+        
+        System.out.println("排序后：");
+        for (int s : sort) {
+            System.out.println(s);
+        }
+    }
+    
+    public static int[] sort(int[] data) {
+        int min;
+        for (int i = 0; i < data.length; i++) {
+            min = i;
+            for (int j = i + 1; j < data.length; j++) {
+                if (data[j] < data[i]) {
+                    min = j;
+                }
+            }
+            if (data[min] < data[i]) {
+                int tmp = data[i];
+                data[i] = data[min];
+                data[min] = tmp;
+            }
+        }
+        return data;
+    }
+    
+}
